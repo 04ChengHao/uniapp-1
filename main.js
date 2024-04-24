6180e4451975bb84c077f13cddf87fdb7cdd70b5
hao <hao@qq.com>
2024-04-24 20:27:04 +0800

    完成了home的开发

--- main.js
+++ main.js
@@ -1,17 +1,39 @@
 import App from './App'
-
-// #ifndef VUE3
-import Vue from 'vue'
+// import Vue from 'vue'
 import './uni.promisify.adaptor'
-Vue.config.productionTip = false
-App.mpType = 'app'
-const app = new Vue({
-  ...App
-})
-app.$mount()
-// #endif
+import {$http} from '@escook/request-miniprogram'
 
-// #ifdef VUE3
+uni.$http = $http
+
+//请求根路径
+$http.baseUrl = "https://api-hmugo-web.itheima.net"
+
+//请求拦截器
+$http.beforeRequest = function(options) {
+	uni.showLoading({
+		title: '数据加载中...'
+	})
+}
+//响应拦截器
+$http.afterRequest = function() {
+	uni.hideLoading();
+}
+// 封装的展示消息提示的方法
+uni.$showMsg = function (title = '数据加载失败！', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none',
+  })
+}
+
+// Vue.config.productionTip = false
+// // App.mpType = 'app'
+// // const app = new Vue({
+// // 	...App
+// // })
+// // app.$mount()
+
 import { createSSRApp } from 'vue'
 export function createApp() {
   const app = createSSRApp(App)
@@ -19,4 +41,3 @@ export function createApp() {
     app
   }
 }
-// #endif
